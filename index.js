111f2a013c7198d4ca685f349d06ab1380a59d19
thuitaf <faith.thuita@moringaschool.com>
2022-03-11 20:41:57 +0300

    commit tree 5f1c4f4a5b47c00874da78de8e0bfb0ee8ae93ec

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Code your solution here
+function findMatching (arr,str) {
+    return arr.filter((match)=> match.toLowerCase()=== str.toLowerCase());
+}
+
+function fuzzyMatch(arr,str){
+
+    return arr.filter((letterBegin)=> letterBegin.indexOf(str) === 0) ;
+
+}
+
+function matchName(arr,str){
+
+    return arr.filter((driverName)=> driverName.name === str)
+
+}
